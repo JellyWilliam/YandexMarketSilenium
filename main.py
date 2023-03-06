58f90f82f9e5520d3abf0efc9c7e924847ddc6d8
Daniel <jellymelly@inbox.ru>
2023-03-06 23:25:42 +0300

    Тест и возможность использования поисковой строки

---
+++ main.py
@@ -0,0 +1,179 @@
+import time
+import tkinter as tk
+from pathlib import Path
+
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class YandexMarket:
+    browser = None
+
+    def __init__(self, search_text="", dataset_name="", max_pages=5):
+        self.init_driver()
+        self.search_text = search_text
+        self.dataset_name = dataset_name
+        self.max_pages = max_pages if max_pages > 0 else float("inf")
+
+    def init_driver(self):
+        options = Options()
+        # Чтобы драйвер не закрывал браузер при ошибке
+        options.add_experimental_option("detach", True)
+        # Чтобы драйвер не выводил свои логи
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        # Подставной юзерагент
+        # options.add_argument(f"user-agent={UserAgent(use_external_data=True).random}")
+        # Запуск в развернутом режиме
+        options.add_argument("--start-maximized")
+        # Путь до драйвера
+        s = Service(str(Path.cwd() / 'yandexdriver.exe'))
+        self.browser = webdriver.Chrome(service=s, options=options)
+
+    def scroll(self):
+        last_height = self.browser.execute_script("return document.body.scrollHeight")
+        while True:
+            self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(0.5)
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+    def get_main_page(self):
+        url_market = "https://market.yandex.ru/"
+        # С помощью метода get открываем страницу
+        self.browser.get(url_market)
+        # Проверяем что мы попали на нужную страницу (возможно потребуется подтвердить вы не робот)
+        WebDriverWait(self.browser, 200).until(
+            EC.title_contains("Интернет-магазин Яндекс Маркет — покупки с быстрой доставкой"))
+
+    def get_element_web_driver_wait(self, selector, by=By.CSS_SELECTOR, delay=10):
+        # Функция, которая проверяет наличие элемента на странице
+        return WebDriverWait(self.browser, delay).until(EC.visibility_of_element_located((by, selector)))
+
+    def get_smartphone(self):
+        catalog_selector = "#catalogPopupButton"
+        smartphone_selector = "#catalogPopup > div > div > div > div > div > div > div.EH4e7 > div > div > div > div._2HhF1 > div > div > div > div > div > div > div > div:nth-child(1) > div:nth-child(1) > ul > li:nth-child(1) > div > a"
+
+        self.get_element_web_driver_wait(catalog_selector).click()
+        self.get_element_web_driver_wait(smartphone_selector).click()
+
+    def next_page(self, n_page):
+        current_title = self.browser.title
+        current_url = self.browser.current_url
+        if current_url.find("&page=") == -1:
+            self.browser.get(f"{current_url}&page={n_page}")
+        else:
+            buf = current_url.split("&page=")
+            self.browser.get(f"{buf[0]}&page={n_page}")
+        new_title = self.browser.title
+        if current_title == new_title:
+            raise Exception("More page not found")
+        time.sleep(2)
+
+    def parce_page(self, dataset_name):
+        name_xpath = lambda \
+                id: f"/html/body/div[4]/div[2]/div[1]/div[5]/div/div/div/div/div/div/div[5]/div/div/div/div/main/div/div/div/div/div/div/div[{id}]/div/div/div/article/div[3]/div[1]/h3/a"
+        parameter_xpath = lambda \
+                id: f"/html/body/div[4]/div[2]/div[1]/div[5]/div/div/div/div/div/div/div[5]/div/div/div/div/main/div/div/div/div/div/div/div[{id}]/div/div/div/article/div[3]/div[2]/ul"
+        price_xpath = lambda \
+                id: f"/html/body/div[4]/div[2]/div[1]/div[5]/div/div/div/div/div/div/div[5]/div/div/div/div/main/div/div/div/div/div/div/div[{id}]/div/div/div/article/div[4]/div[1]/div[1]/div/a/div/span/span[1]"
+
+        name_arr = []
+        parameter_arr = []
+        price_arr = []
+
+        n_page = 2
+        while True:
+            try:
+                self.scroll()
+                for i in range(1, 50):
+                    try:
+                        name_arr.append(self.get_element_web_driver_wait(name_xpath(i), By.XPATH).text)
+                        parameter_arr.append(self.get_element_web_driver_wait(parameter_xpath(i), By.XPATH).text)
+                        price_arr.append(self.get_element_web_driver_wait(price_xpath(i), By.XPATH).text)
+                    except Exception as e:
+                        print(f"ID {i} - выдал ошибку:\n{e}")
+                        break
+                if n_page > self.max_pages:
+                    break
+                self.next_page(n_page)
+                n_page += 1
+            except Exception as e:
+                print(e)
+                break
+        self.browser.execute_script("window.scrollTo(0, 0);")
+        pd.DataFrame({"name": name_arr, "parameter": parameter_arr, "price": price_arr}).to_csv(f"{dataset_name}.csv")
+        print("Парсинг данных завершен!")
+
+    def search(self, text):
+        search_selector = "#header-search"
+
+        self.get_element_web_driver_wait(search_selector).send_keys(text)
+        self.get_element_web_driver_wait(search_selector).send_keys(Keys.ENTER)
+        time.sleep(2)
+
+    def test(self):
+        self.get_main_page()
+        self.get_smartphone()
+        self.parce_page("Парсинг смартфонов")
+
+    def run(self):
+        self.get_main_page()
+        self.search(self.search_text)
+        self.parce_page(self.dataset_name)
+
+
+def gui_init():
+    window = tk.Tk()
+    window.title("Парсинг страниц с ЯндексМаркета")
+    greeting = tk.Label(text="Параметры")
+    greeting.pack()
+
+    search_text = tk.StringVar()
+
+    dataset_name = tk.StringVar()
+
+    number_page = tk.IntVar()
+
+    tk.Label(text="Поиск").pack()
+    tk.Entry(textvariable=search_text).pack()
+
+    tk.Label(text="Имя датасета").pack()
+    tk.Entry(textvariable=dataset_name).pack()
+
+    tk.Label(text="Сколько страниц пройти (0 - все возможные)").pack()
+    tk.Entry(textvariable=number_page).pack()
+
+    def start_button(test=False):
+        if test:
+            YandexMarket().test()
+        else:
+            YandexMarket(search_text=search_text.get(), dataset_name=dataset_name.get(),
+                         max_pages=number_page.get()).run()
+
+    tk.Button(
+        text="Тест",
+        command=lambda: start_button(True),
+        width=50,
+        height=2,
+    ).pack()
+
+    tk.Button(
+        text="Запуск",
+        command=start_button,
+        width=50,
+        height=2,
+    ).pack()
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    gui_init()
