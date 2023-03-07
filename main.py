0bd83784a2a19993d3fe4fe4da3532aa6bfa5c62
Daniel <jellymelly@inbox.ru>
2023-03-07 10:51:28 +0300

    Теперь можно после выполнения одного поиска сразу запускать другой

--- main.py
+++ main.py
@@ -15,11 +15,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 class YandexMarket:
     browser = None
 
-    def __init__(self, search_text="", dataset_name="", max_pages=5):
+    def __init__(self):
         self.init_driver()
-        self.search_text = search_text
-        self.dataset_name = dataset_name
-        self.max_pages = max_pages if max_pages > 0 else float("inf")
 
     def init_driver(self):
         options = Options()
@@ -36,13 +33,19 @@ class YandexMarket:
         self.browser = webdriver.Chrome(service=s, options=options)
 
     def scroll(self):
+        # Скрипт, который получает высоту страницы
         last_height = self.browser.execute_script("return document.body.scrollHeight")
         while True:
+            # Пролистывание страницы вниз (для прогрузки всех товаров на странице)
             self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            # Ожидание
             time.sleep(0.5)
+            # Получение новой высоты страницы (на тот случай, если произойдет ещё подгрузка данных)
             new_height = self.browser.execute_script("return document.body.scrollHeight")
+            # Сравнение высоты страницы до и после, если они равны, то выходим из цикла
             if new_height == last_height:
                 break
+            # Если высоты страниц не равны, то перезаписываем высоту и продолжаем скролить вниз
             last_height = new_height
 
     def get_main_page(self):
@@ -54,30 +57,43 @@ class YandexMarket:
             EC.title_contains("Интернет-магазин Яндекс Маркет — покупки с быстрой доставкой"))
 
     def get_element_web_driver_wait(self, selector, by=By.CSS_SELECTOR, delay=10):
-        # Функция, которая проверяет наличие элемента на странице
+        # Функция, которая проверяет наличие элемента на странице и возвращает его, если он есть
         return WebDriverWait(self.browser, delay).until(EC.visibility_of_element_located((by, selector)))
 
     def get_smartphone(self):
+        # CSS Selector кнопки каталога
         catalog_selector = "#catalogPopupButton"
+        # Selector выбора смрартфонов из каталога
         smartphone_selector = "#catalogPopup > div > div > div > div > div > div > div.EH4e7 > div > div > div > div._2HhF1 > div > div > div > div > div > div > div > div:nth-child(1) > div:nth-child(1) > ul > li:nth-child(1) > div > a"
 
+        # Эмитирование нажатия правой кнопки на элемент методом .click()
         self.get_element_web_driver_wait(catalog_selector).click()
         self.get_element_web_driver_wait(smartphone_selector).click()
 
     def next_page(self, n_page):
+        # Получение текущего тайтла старницы
         current_title = self.browser.title
+        # Получение текущего url страницы
         current_url = self.browser.current_url
+        # Проверка что в ссылке есть параметр номера старницы
         if current_url.find("&page=") == -1:
+            # Если параметра нет, то добавляем его
             self.browser.get(f"{current_url}&page={n_page}")
         else:
+            # Если он есть, то делаем замену номера траницы
             buf = current_url.split("&page=")
             self.browser.get(f"{buf[0]}&page={n_page}")
+        # Поулчаем текущий тайтл
         new_title = self.browser.title
+        # Сравниваем с прошлым тайтлом (если страницы 2, а мы хотим 3, то в тайтле отсанется сатрница 2)
         if current_title == new_title:
+            # Вывод ошибки, что страница не поменялась, следовательно больше страниц нет
             raise Exception("More page not found")
+        # Ожидание подгрузки страницы
         time.sleep(2)
 
-    def parce_page(self, dataset_name):
+    def parce_page(self, dataset_name, max_pages):
+        # Лямбда функции получения XPATH каждого итема на странице
         name_xpath = lambda \
                 id: f"/html/body/div[4]/div[2]/div[1]/div[5]/div/div/div/div/div/div/div[5]/div/div/div/div/main/div/div/div/div/div/div/div[{id}]/div/div/div/article/div[3]/div[1]/h3/a"
         parameter_xpath = lambda \
@@ -85,6 +101,7 @@ class YandexMarket:
         price_xpath = lambda \
                 id: f"/html/body/div[4]/div[2]/div[1]/div[5]/div/div/div/div/div/div/div[5]/div/div/div/div/main/div/div/div/div/div/div/div[{id}]/div/div/div/article/div[4]/div[1]/div[1]/div/a/div/span/span[1]"
 
+        # Массивы для записи всех данных
         name_arr = []
         parameter_arr = []
         price_arr = []
@@ -95,39 +112,52 @@ class YandexMarket:
                 self.scroll()
                 for i in range(1, 50):
                     try:
+                        # Получение текста элемента
                         name_arr.append(self.get_element_web_driver_wait(name_xpath(i), By.XPATH).text)
                         parameter_arr.append(self.get_element_web_driver_wait(parameter_xpath(i), By.XPATH).text)
                         price_arr.append(self.get_element_web_driver_wait(price_xpath(i), By.XPATH).text)
                     except Exception as e:
                         print(f"ID {i} - выдал ошибку:\n{e}")
-                        break
-                if n_page > self.max_pages:
+                        if i != 1:
+                            break
+                if n_page > max_pages:
                     break
                 self.next_page(n_page)
                 n_page += 1
             except Exception as e:
                 print(e)
                 break
+        # Скролл старницы вверх
         self.browser.execute_script("window.scrollTo(0, 0);")
+        # Создание и запись датафрейма в файл
         pd.DataFrame({"name": name_arr, "parameter": parameter_arr, "price": price_arr}).to_csv(f"{dataset_name}.csv")
         print("Парсинг данных завершен!")
 
     def search(self, text):
+        # CSS Selector поля ввода
         search_selector = "#header-search"
-
+        # Отправка строки в поле поиска
         self.get_element_web_driver_wait(search_selector).send_keys(text)
+        # Нажатие кнопки Enter
         self.get_element_web_driver_wait(search_selector).send_keys(Keys.ENTER)
+        # Ожидание прогрузки страницы
         time.sleep(2)
 
     def test(self):
+        # Функция для теста
         self.get_main_page()
         self.get_smartphone()
-        self.parce_page("Парсинг смартфонов")
+        self.parce_page("Парсинг смартфонов", 3)
 
-    def run(self):
+    def run(self, search_text, dataset_name, max_pages):
+        # Полноценная работа с поиском
+        max_pages = max_pages if max_pages > 0 else float("inf")
         self.get_main_page()
-        self.search(self.search_text)
-        self.parce_page(self.dataset_name)
+        self.search(search_text)
+        self.parce_page(dataset_name, max_pages)
+
+
+yandex_market = None
 
 
 def gui_init():
@@ -152,11 +182,14 @@ def gui_init():
     tk.Entry(textvariable=number_page).pack()
 
     def start_button(test=False):
+        global yandex_market
+        if yandex_market is None:
+            yandex_market = YandexMarket()
         if test:
-            YandexMarket().test()
+            yandex_market.test()
         else:
-            YandexMarket(search_text=search_text.get(), dataset_name=dataset_name.get(),
-                         max_pages=number_page.get()).run()
+            yandex_market.run(search_text=search_text.get(), dataset_name=dataset_name.get(),
+                              max_pages=number_page.get())
 
     tk.Button(
         text="Тест",
